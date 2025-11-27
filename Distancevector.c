8c81c9fbffa83a68f3cedbbb12767dc71a58a5a1
Sure-Mohan <151706255+Sure-Mohan@users.noreply.github.com>
2025-11-27 22:33:34 +0530

    Distancevector.c

---
+++ Distancevector.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define INF 999
+#define MAX 10
+int main() {
+    int n, i, j, k, updated;
+    int cost[MAX][MAX], dist[MAX][MAX];
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+    printf("Enter adjacency matrix (999 for INF):\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            scanf("%d", &cost[i][j]);
+    // Initialize distance table
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            dist[i][j] = cost[i][j];
+    // Distance Vector Algorithm
+    do {
+        updated = 0;
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++)
+                for (k = 0; k < n; k++)
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        updated = 1;
+                    }
+    } while (updated);
+    // Print final distance table
+    printf("\nFinal Distance Table:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++)
+            printf("%d ", dist[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
